f1645bc23689ad4b8ea4d7c1d0341d6e10b1198a
Jonathan Chan <johnkchan252@gmail.com>
2018-08-16 00:37:30 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
 # code here!
+class School
+  attr_reader :roster
+  
+  def initialize()
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    @roster[grade] << name
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+end
